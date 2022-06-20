54a0d9660f28d1ed74c0ac9e5907569fd035621a
Shabbir Hussain <95009001+Shabbir-9916@users.noreply.github.com>
2022-06-20 09:34:23 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import torch
+from PIL import Image
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import datasets, transforms, models
+
+
+def predict_image(image):
+    image_tensor = test_transforms(image).float()
+    image_tensor = image_tensor.unsqueeze_(0)
+    input = Variable(image_tensor)
+    input = input.to(device)
+    output = model(input)
+    index = output.data.cpu().numpy().argmax()
+    print("The conf value is : ", np.exp(output.data.cpu().numpy().max()))
+    return index
+
+test_transforms = transforms.Compose([transforms.Resize((224, 224)),
+                                      transforms.ToTensor(),
+                                     ])
+
+to_pil = transforms.ToPILImage()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model=torch.load('material_nomet_model.pth')
+model=torch.load('ckps/resnet_material_fullmodel.pth')
+model.eval()
+
+
+im = Image.open('/home/ubuntu/material_project/Test_Images/hickory.jpg')
+index = predict_image(im)
+
+mat_classes = ['Bricks', 'fabric', 'foliage', 'glass', 'leather', 'metal', 'paper', 'plastic', 'stone', 'water', 'wood']
+
+print("The material prediction is : ", mat_classes[index])
